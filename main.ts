04ee9dbfa9522b5e86bbff5dace7d5978922241c
Quy-An <quyan18b@gmail.com>
2025-01-23 16:24:25 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,13 +9,15 @@ namespace AnBuildExtension{
         led.plot(x, y)
     }
 
-    //% block="send I2C data: $data1, $data2, $data3, $data4, $data5, $data6"
-    export function sendI2CData(data1: number, data2: number, data3: number, data4: number, data5: number, data6: number): void {
+    //% block="send I2C data: $addr $data1, $data2, $data3, $data4, $data5, $data6"
+    export function sendI2CData(addr: number, data1: number, data2: number, data3: number, data4: number, data5: number, data6: number): void {
         let dataToSend: number[] = [data1, data2, data3, data4, data5, data6];
         let buffer = pins.createBuffer(dataToSend.length);
         for (let i = 0; i < dataToSend.length; i++) {
             buffer.setUint8(i, dataToSend[i]);
         }
-        pins.i2cWriteBuffer(0x40, buffer);
+        pins.i2cWriteBuffer(addr, buffer);
     }
+
+
 }
